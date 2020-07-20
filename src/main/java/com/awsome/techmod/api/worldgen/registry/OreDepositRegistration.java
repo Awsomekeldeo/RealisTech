55f309acfc688277210171da968df753f999b3f6
Awsomekeldeo <logster62@gmail.com>
2020-07-20 00:55:51 -0700

    Added vanilla ores to worldgen.

--- src/main/java/com/awsome/techmod/api/worldgen/registry/OreDepositRegistration.java
+++ src/main/java/com/awsome/techmod/api/worldgen/registry/OreDepositRegistration.java
@@ -12,12 +12,19 @@ import scala.actors.threadpool.Arrays;
 public class OreDepositRegistration {
 	@SuppressWarnings("unchecked")
 	public static void init() {
-		OreAPI.registerMineralDeposit(ModBlocks.copperOre.getDefaultState(), ModBlocks.copperSample, 32, 112, 50, 5, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "malachite");
-		OreAPI.registerMineralDeposit(ModBlocks.tinOre.getDefaultState(), ModBlocks.tinSample, 32, 112, 45, 5, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "cassiterite");
-		OreAPI.registerMineralDeposit(ModBlocks.nickelOre.getDefaultState(), ModBlocks.nickelSample, 16, 48, 30, 4, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "garnierite");
-		OreAPI.registerMineralDeposit(ModBlocks.leadOre.getDefaultState(), ModBlocks.leadSample, 16, 32, 40, 5, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "galena");
-		OreAPI.registerMineralDeposit(ModBlocks.silverOre.getDefaultState(), ModBlocks.silverSample, 16, 32, 30, 4, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "native_silver");
-		OreAPI.registerMineralDeposit(ModBlocks.zincOre.getDefaultState(), ModBlocks.zincSample, 24, 56, 20, 3, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "sphalerite");
-		OreAPI.registerMineralDeposit(ModBlocks.cobaltOre.getDefaultState(), ModBlocks.cobaltSample, 16, 32, 50, 2, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "cobaltite");
+		OreAPI.registerMineralDeposit(ModBlocks.copperOre.getDefaultState(), ModBlocks.copperSample, 25, 52, 50, 36, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "malachite");
+		OreAPI.registerMineralDeposit(ModBlocks.tinOre.getDefaultState(), ModBlocks.tinSample, 44, 68, 45, 37, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "cassiterite");
+		OreAPI.registerMineralDeposit(ModBlocks.nickelOre.getDefaultState(), ModBlocks.nickelSample, 6, 40, 30, 17, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "garnierite");
+		OreAPI.registerMineralDeposit(ModBlocks.leadOre.getDefaultState(), ModBlocks.leadSample, 16, 50, 40, 19, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "galena");
+		OreAPI.registerMineralDeposit(ModBlocks.silverOre.getDefaultState(), ModBlocks.silverSample, 16, 50, 30, 19, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "native_silver");
+		OreAPI.registerMineralDeposit(ModBlocks.zincOre.getDefaultState(), ModBlocks.zincSample, 35, 55, 20, 19, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "sphalerite");
+		OreAPI.registerMineralDeposit(ModBlocks.cobaltOre.getDefaultState(), ModBlocks.cobaltSample, 16, 32, 50, 15, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "cobaltite");
+		OreAPI.registerMineralDeposit(ModBlocks.modCoalOre.getDefaultState(), ModBlocks.coalSample, 28, 78, 40, 23, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "bituminous_coal");
+		OreAPI.registerMineralDeposit(ModBlocks.modIronOre.getDefaultState(), ModBlocks.ironSample, 32, 60, 50, 22, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "hematite");
+		OreAPI.registerMineralDeposit(ModBlocks.modGoldOre.getDefaultState(), ModBlocks.goldSample, 5, 40, 35, 19, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "native_gold");
+		OreAPI.registerMineralDeposit(ModBlocks.modDiamondOre.getDefaultState(), ModBlocks.diamondSample, 2, 20, 20, 15, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "kimberlite");
+		OreAPI.registerMineralDeposit(ModBlocks.modRedstoneOre.getDefaultState(), ModBlocks.redstoneSample, 5, 12, 35, 17, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "cinnabar");
+		OreAPI.registerMineralDeposit(ModBlocks.modEmeraldOre.getDefaultState(), ModBlocks.emeraldSample, 4, 32, 20, 15, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "beryl");
+		OreAPI.registerMineralDeposit(ModBlocks.modLapisOre.getDefaultState(), ModBlocks.lapisSample, 10, 38, 35, 31, new int[] { 1 }, new ArrayList<IBlockState>(Arrays.asList(new IBlockState[] {Blocks.STONE.getDefaultState()})), 0.5f, "lazurite");
 	}
 }
--- src/main/java/com/awsome/techmod/blocks/BlockModOre.java
+++ src/main/java/com/awsome/techmod/blocks/BlockModOre.java
@@ -3,8 +3,11 @@ package com.awsome.techmod.blocks;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import com.awsome.techmod.Reference;
+import com.awsome.techmod.Techmod;
+import com.awsome.techmod.api.ModItems;
 import com.awsome.techmod.inventory.ModTabs;
 
 import net.minecraft.block.Block;
@@ -13,6 +16,7 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
@@ -47,7 +51,7 @@ public class BlockModOre extends Block {
 		this.setDefaultState(this.blockState.getBaseState().withProperty(STONETYPE, BlockModOre.EnumStoneType.STONE));
 		ORES.add(this);
 	}
-	
+
 	public IBlockState getStateFromMeta(int meta){
         return this.getDefaultState().withProperty(STONETYPE, BlockModOre.EnumStoneType.values()[meta]);
     }
@@ -110,10 +114,57 @@ public class BlockModOre extends Block {
 	@Override
 	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state,
 			int fortune) {
+		Random random = new Random();
+		String blockName = this.getRegistryName().toString();
+		if (blockName.equals("techmod:mod_coal_ore")) {
+			drops.add(new ItemStack(Blocks.COAL_ORE.getItemDropped(state, random, fortune), Blocks.COAL_ORE.quantityDroppedWithBonus(fortune, random), Blocks.COAL_ORE.damageDropped(state)));
+			Techmod.logger.info(blockName);
+		}else if ((blockName.equals("techmod:mod_redstone_ore"))) {
+			drops.add(new ItemStack(Blocks.REDSTONE_ORE.getItemDropped(state, random, fortune), Blocks.REDSTONE_ORE.quantityDroppedWithBonus(fortune, random), Blocks.REDSTONE_ORE.damageDropped(state)));
+		}else if ((blockName.equals("techmod:mod_diamond_ore"))) {
+			drops.add(new ItemStack(Blocks.DIAMOND_ORE.getItemDropped(state, random, fortune), Blocks.DIAMOND_ORE.quantityDroppedWithBonus(fortune, random), Blocks.DIAMOND_ORE.damageDropped(state)));
+		}else if ((blockName.equals("techmod:mod_emerald_ore"))) {
+			drops.add(new ItemStack(Blocks.EMERALD_ORE.getItemDropped(state, random, fortune), Blocks.EMERALD_ORE.quantityDroppedWithBonus(fortune, random), Blocks.EMERALD_ORE.damageDropped(state)));
+		}else if ((blockName.equals("techmod:mod_lapis_ore"))) {
+			drops.add(new ItemStack(Blocks.LAPIS_ORE.getItemDropped(state, random, fortune), Blocks.LAPIS_ORE.quantityDroppedWithBonus(fortune, random), Blocks.LAPIS_ORE.damageDropped(state)));
+		}else if ((blockName.equals("techmod:mod_iron_ore"))) {
+			drops.add(new ItemStack(ModItems.ironCluster, 1, 0));
+		}else if ((blockName.equals("techmod:mod_gold_ore"))) {
+			drops.add(new ItemStack(ModItems.goldCluster, 1, 0));
+		}else if ((blockName.equals("techmod:copper_ore"))) {
+			drops.add(new ItemStack(ModItems.copperCluster, 1, 0));
+		}else if ((blockName.equals("techmod:tin_ore"))) {
+			drops.add(new ItemStack(ModItems.tinCluster, 1, 0));
+		}else if ((blockName.equals("techmod:nickel_ore"))) {
+			drops.add(new ItemStack(ModItems.nickelCluster, 1, 0));
+		}else if ((blockName.equals("techmod:lead_ore"))) {
+			drops.add(new ItemStack(ModItems.leadCluster));
+		}else if ((blockName.equals("techmod:cobalt_ore"))) {
+			drops.add(new ItemStack(ModItems.cobaltCluster));
+		}else if ((blockName.equals("techmod:zinc_ore"))) {
+			drops.add(new ItemStack(ModItems.zincCluster));
+		}else if ((blockName.equals("techmod:silver_ore"))) {
+			drops.add(new ItemStack(ModItems.silverCluster));
+		}
 		
-		for (int i = 0; i < dropList.size(); i++) {
-			ItemStack j = dropList.get(i);
-			drops.add(j);
+		
+	}
+
+	@Override
+	public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
+		String blockName = this.getRegistryName().toString();
+		if (blockName.equals("techmod:mod_coal_ore")) {
+			return Blocks.COAL_ORE.getExpDrop(state, world, pos, fortune);
+		}else if ((blockName.equals("techmod:mod_redstone_ore"))) {
+			return Blocks.REDSTONE_ORE.getExpDrop(state, world, pos, fortune);
+		}else if ((blockName.equals("techmod:mod_diamond_ore"))) {
+			return Blocks.DIAMOND_ORE.getExpDrop(state, world, pos, fortune);
+		}else if ((blockName.equals("techmod:mod_emerald_ore"))) {
+			return Blocks.EMERALD_ORE.getExpDrop(state, world, pos, fortune);
+		}else if ((blockName.equals("techmod:mod_lapis_ore"))) {
+			return Blocks.LAPIS_ORE.getExpDrop(state, world, pos, fortune);
+		}else{
+			return 0;
 		}
 	}
 }
